e8678fdac9a0934d1483071803442beab428a5c3
Evgeny Kovalevgit config --global user.name Evgeny <kovalevee@bk.ru>
2024-04-09 15:13:29 +0300

    fix

--- app/Services/Messages/Provider/Reddy/Reddy.php
+++ app/Services/Messages/Provider/Reddy/Reddy.php
@@ -2,6 +2,7 @@
 
 namespace App\Services\Messages\Provider\Reddy;
 
+use App\Models\Bots;
 use App\Services\Messages\Provider\MessageProviderInterface;
 use Illuminate\Http\Client\Response;
 use Illuminate\Support\Facades\Http;
@@ -9,12 +10,12 @@ use Illuminate\Support\Facades\Http;
 class Reddy implements MessageProviderInterface
 {
 
-    protected $token = '3cuwl3-JDAG-fbBWdKVaxqjR_m3w6pC0';
-    public function send(mixed $receiver, string $message, string $endpoint, string $messageType) : Response
+
+    public function send(mixed $receiver, Bots $bot, string $message, string $endpoint, string $messageType) : Response
     {
         $response = Http::asForm(
         )->post(
-            'https://bot.reddy.team/bot' . $this->token . '/' . $endpoint,
+            'https://bot.reddy.team/bot' . $bot->token . '/' . $endpoint,
             [
                 'msg' => $message,
                 'userkey' => $receiver,
